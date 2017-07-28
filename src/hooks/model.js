f5caa404d38ecfa7103d5cd128eaaf76c826f3a6
Luc Claustres <luc.claustres@orange.fr>
2017-07-28 18:00:55 +0200

    Fixed missing hook file

---
+++ src/hooks/model.js
@@ -0,0 +1,22 @@
+import _ from 'lodash'
+import { discard } from 'feathers-hooks-common'
+// import makeDebug from 'debug'
+
+// const debug = makeDebug('kaelia:kCore')
+
+export function processPerspectives (hook) {
+  let params = hook.params
+  let query = params.query
+  let service = hook.service
+
+  // Test if some perspectives are defined on the model
+  if (!service.perspectives) return
+
+  // Iterate through known perspectives of the model
+  service.perspectives.forEach(perspective => {
+    // Only discard if not explicitely asked by $select
+    if (_.isNil(query) || _.isNil(query.$select) || !query.$select.includes(perspective)) {
+      discard(perspective)(hook)
+    }
+  })
+}
