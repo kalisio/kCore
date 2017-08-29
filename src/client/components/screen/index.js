074c43b785a139b54484b24030dd694b3cae19ae
christophe.nouguier@free.fr <christophe.nouguier@free.fr>
2017-08-29 15:57:01 +0200

    Removed ScreenHeader/Footer export

--- src/client/components/screen/index.js
+++ src/client/components/screen/index.js
@@ -1,9 +1,5 @@
 import KScreen from './KScreen.vue'
-import KScreenHeader from './KScreenHeader.vue'
-import KScreenFooter from './KScreenFooter.vue'
 
 export {
-  KScreen,
-  KScreenHeader,
-  KScreenFooter
+  KScreen
 }
