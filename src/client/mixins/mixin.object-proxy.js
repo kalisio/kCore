f195f07845204a967c24145cee7944e98ba0ec7c
Luc Claustres <luc.claustres@orange.fr>
2017-11-22 09:27:07 +0100

    Object proxy now resolves in updated hook instead of created so that $refs are more likely to be available

--- src/client/mixins/mixin.object-proxy.js
+++ src/client/mixins/mixin.object-proxy.js
@@ -13,31 +13,34 @@ let objectProxyMixin = {
     }
   },
   methods: {
+    hasObject () {
+      return !lodash.isEmpty(this.id) && this.getService()
+    },
     getObject () {
       return this._object
     },
     loadObject () {
-      this._object = null
-      if (!lodash.isEmpty(this.id)) {
-        if (this.getService()) {
-          let params = {}
-          if (this.perspective) {
-            params = { query: { $select: [this.perspective] } }
-          }
-          this.getService().get(this.id, params)
-          .then(values => {
-            this._object = values
-            this.$emit('object-changed', this._object)
-          })
-        } else {
-          logger.warn('Invalid service')
-        }
+      this.isLoading = true
+      let params = {}
+      if (this.perspective) {
+        params = { query: { $select: [this.perspective] } }
       }
+      this.getService().get(this.id, params)
+      .then(values => {
+        this.isLoading = false
+        this._object = values
+        this.$emit('object-changed', this._object)
+      })
+    },
+    setObject (object) {
+      this._object = object
       this.$emit('object-changed', this._object)
     }
   },
-  created () {
-    this.loadObject()
+  updated () {
+    if (this.hasObject() && !this.isLoading) {
+      this.loadObject()
+    }
   }
 }
 
