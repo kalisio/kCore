bbd523cdc5c5e603d7bbc271ee36251f70c66718
Luc Claustres <luc.claustres@orange.fr>
2017-12-15 10:51:18 +0100

    Added index to user collection

--- src/models/users.model.mongodb.js
+++ src/models/users.model.mongodb.js
@@ -1,4 +1,5 @@
 module.exports = function (app, options) {
   options.Model = app.db.collection('users')
   options.Model.ensureIndex({ email: 1 }, { unique: true })
+  options.Model.ensureIndex({ 'profile.name': 1 })
 }
