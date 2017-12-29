0bce91fe55691b472c84fb16d93df4929c90d22b
Christophe Nouguier <christophe.nouguier@free.fr>
2017-12-29 17:32:28 +0100

    Cleaned index.js

--- src/client/components/frame/index.js
+++ src/client/components/frame/index.js
@@ -1,11 +1,9 @@
 import KBlock from './KBlock.vue'
-import KDialog from './KDialog.vue'
 import KModal from './KModal.vue'
 import KScreen from './KScreen.vue'
 
 export {
   KBlock,
-  KDialog,
   KModal,
   KScreen
 }
