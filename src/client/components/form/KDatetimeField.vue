1f5766c1f879467fe82872088c023cb27d2f9b9d
Christophe Nouguier <christophe.nouguier@free.fr>
2018-01-01 16:46:38 +0100

    Updated to Quasar 0.14.7 Added KDatetimeField

---
+++ src/client/components/form/KDatetimeField.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-field
+    :icon="icon"
+    :label="label"
+    :helper="helper"
+    :error-label="errorLabel"
+    :label-width="labelWidth"
+    :error="hasError"
+  >
+    <q-datetime
+      v-model="model"
+      v-bind="properties.field"
+      @blur="onChanged" />
+  </q-field>
+</template>
+
+<script>
+import { QField, QDatetime } from 'quasar'
+import mixins from '../../mixins'
+
+export default {
+  name: 'k-datetime-field',
+  components: {
+    QField,
+    QDatetime
+  },
+  mixins: [mixins.baseField]
+}
+</script>
