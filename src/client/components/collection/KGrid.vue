9a52394ca5cff958ec29c31e79ddf56becd41b6c
Christophe Nouguier <christophe.nouguier@free.fr>
2018-01-02 17:34:14 +0100

    Updated KGrid to use a renderer prop of type of Object

--- src/client/components/collection/KGrid.vue
+++ src/client/components/collection/KGrid.vue
@@ -2,7 +2,7 @@
   <div class="column justify-center"> 
     <div class="row">
       <div v-for="item in items" :key="item._id" :class="layout">
-        <k-renderer :item="item" :options="rendererOptions" :actions="actions" />
+        <k-renderer :item="item" :options="rendererOptions" :actions="actions" v-bind="renderer.props" />
       </div>
     </div>
     <div class="self-center" v-if="nbPages > 1">
@@ -24,11 +24,11 @@ export default {
   props: {
     layout: {
       type: String,
-      default: 'col-xs-12 col-sm-6 col-lg-4 col-xl-3'
+      default: 'col-xs-6 col-sm-4 col-lg-4 col-xl-6'
     },
     renderer: {
-      type: String,
-      default: 'collection/KCard'
+      type: Object,
+      default: () => { return { component: 'collection/KCard', props: {} } }
     },
     rendererOptions: {
       type: Object,
@@ -43,7 +43,7 @@ export default {
   },
   created () {
     // Load the component
-    this.$options.components['k-renderer'] = this.$load(this.renderer)
+    this.$options.components['k-renderer'] = this.$load(this.renderer.component)
     this.refreshCollection()
   }
 }
