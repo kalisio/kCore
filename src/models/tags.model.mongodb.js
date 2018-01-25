d7ccda43340413d9b953452e67912f00254ac705
Luc Claustres <luc.claustres@orange.fr>
2018-01-25 09:17:47 +0100

    Fixed tag index in DB

--- src/models/tags.model.mongodb.js
+++ src/models/tags.model.mongodb.js
@@ -1,6 +1,6 @@
 module.exports = function (app, options) {
   let db = options.db || app.db
   options.Model = db.collection('tags')
-  options.Model.ensureIndex({ value: 1 }, { unique: true })
-  options.Model.ensureIndex({ scope: 1 })
+  // Use compound index to have unique pairs scope/value
+  options.Model.ensureIndex({ scope: 1, value: 1 }, { unique: true })
 }
