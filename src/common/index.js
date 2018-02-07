65719704b5e9d65be059a0603446687929c14723
Luc Claustres <luc.claustres@orange.fr>
2018-02-07 16:24:23 +0100

    Added errors

--- src/common/index.js
+++ src/common/index.js
@@ -1,4 +1,5 @@
 // We faced a bug in babel so that transform-runtime with export * from 'x' generates import statements in transpiled code
 // Tracked here : https://github.com/babel/babel/issues/2877
 // We tested the workaround given here https://github.com/babel/babel/issues/2877#issuecomment-270700000 with success so far
+export * as errors from './errors'
 export * as permissions from './permissions'
