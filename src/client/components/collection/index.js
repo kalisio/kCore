10becc06224edda592a795fadcabb753a47e47cf
Christophe Nouguier <christophe.nouguier@free.fr>
2018-03-01 13:35:12 +0100

    Removed unused components replaced by KSearchBar. Fix #25

--- src/client/components/collection/index.js
+++ src/client/components/collection/index.js
@@ -1,13 +1,9 @@
 import KCard from './KCard.vue'
-import KPatternFilter from './KPatternFilter.vue'
-import KTagFilter from './KTagFilter.vue'
 import KGrid from './KGrid.vue'
 import KItem from './KItem.vue'
 
 export {
   KCard,
-  KPatternFilter,
-  KTagFilter,
   KGrid,
   KItem
 }
