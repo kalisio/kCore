3f62bdd3edac2bf418e1530b090b0e280e2cde61
Luc Claustres <luc.claustres@orange.fr>
2018-05-17 16:48:21 +0200

    WIP https://github.com/kalisio/aktnmap/issues/2 (expiry date on users)

--- src/models/users.model.mongodb.js
+++ src/models/users.model.mongodb.js
@@ -2,4 +2,6 @@ module.exports = function (app, options) {
   options.Model = app.db.collection('users')
   options.Model.ensureIndex({ email: 1 }, { unique: true })
   options.Model.ensureIndex({ 'profile.name': 1 })
+  // Inactive user account might expire at a given date
+  options.Model.ensureIndex({ expireAt: 1 }, { expireAfterSeconds: 0 })
 }
