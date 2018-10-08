32e6b0b8c8ee35d6e2b94b4ea27712f45889fba3
Christophe Nouguier <christophe.nouguier@free.fr>
2018-10-08 12:05:15 +0200

    WIP #23

---
+++ src/client/components/input/KInputFile.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <q-input 
+      v-model="fileName" 
+      type="file" 
+      :attributes="{id: 'input'}" 
+      v-bind="$attrs"
+      @change="onChanged" />
+  </div>
+</template>
+
+<script>
+import { QInput } from 'quasar'
+
+export default {
+  name: 'k-input-file',
+  components: {
+    QInput
+  },
+  props: {
+     mimeTypes: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      fileName: ""
+    }
+  },
+  methods: {
+    onChanged (newFileName) {
+      if (newFileName === "") {
+        this.$emit('cleared')
+      } else {
+        let file = document.getElementById("input").files[0]
+        if (!this.mimeTypes.includes(file.type)) this.$emit('rejected')
+        else {
+          let reader = new FileReader()
+          reader.addEventListener('loadend', () => this.$emit('loaded', reader.result))
+          reader.addEventListener('error', () => this.$emit('failed'))
+          reader.readAsText(file)      
+        }   
+      } 
+    }
+  },
+  mounted () {
+    console.log(this.$props)
+    console.log(this.$attrs)
+  }
+}
+</script>
--- src/client/components/input/index.js
+++ src/client/components/input/index.js
@@ -2,6 +2,7 @@ import KAutocomplete from './KAutocomplete.vue'
 import KPalette from './KPalette.vue'
 import KIconChooser from './KIconChooser.vue'
 import KItemChooser from './KItemChooser.vue'
+import KInputFile from './KInputFile.vue'
 import KUploader from './KUploader.vue'
 
 export {
@@ -9,5 +10,6 @@ export {
   KPalette,
   KIconChooser,
   KItemChooser,
+  KInputFile,
   KUploader
 }
