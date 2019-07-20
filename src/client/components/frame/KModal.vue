e356855b0db3b5d40f804cc20f102601759a1bc1
leob <leobdev@gmail.com>
2019-07-20 10:16:58 +0800

    WIP https://github.com/kalisio/kdk/issues/31 (added KBtn back for correct loading behavior, after discussing it with Luc)

--- src/client/components/frame/KModal.vue
+++ src/client/components/frame/KModal.vue
@@ -34,9 +34,9 @@
         <slot name="dialog-actions">
           <div class="row justify-end">
             <template v-for="button in buttons">
-              <q-btn :id="button.name" v-bind:key="button.name" loader flat color="primary" @click="button.handler">
+              <k-btn :id="button.name" :key="button.name" flat color="primary" @click="button.handler">
                 {{button.label}}
-              </q-btn>
+              </k-btn>
             </template>
           </div>
         </slot>
@@ -46,9 +46,13 @@
 </template>
 
 <script>
+import { KBtn } from '../input'
 
 export default {
   name: 'k-modal',
+  components: {
+    KBtn
+  },
   props: {
     title: {
       type: String,
