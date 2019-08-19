3b6df7e8105831c7fa646c6c271a74c798700679
Christophe Nouguier <christophe.nouguier@free.fr>
2019-08-19 16:27:39 +0200

    wip: https://github.com/kalisio/kdk/issues/31 (try to fix platform detection)

--- src/client/api.js
+++ src/client/api.js
@@ -135,9 +135,9 @@ export function kalisio () {
   }
 
   api.getBaseUrl = function () {
-    // FIXME: for test
-    let isMobile = Platform.is.cordova || Platform.is.ios
-    let origin = isMobile ? config.domain : window.location.origin
+    
+    // let isMobile = Platform.is.cordova || Platform.is.ios
+    let origin = Platform.is.cordova ? config.domain : window.location.origin
     // Check for registered custom base Url if any
     if (window.localStorage.getItem(getBaseUrlStorageKey())) {
       origin = window.localStorage.getItem(getBaseUrlStorageKey())
